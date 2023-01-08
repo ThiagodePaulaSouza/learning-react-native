2fff628a3218c45a05f52b8d4a6fe22eea9baff1
ThiagoDePaulaSouza <thiagodepaulasouza@gmail.com>
2023-01-08 14:49:30 -0300

    estrutura base

---
+++ learningReactNative/App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Finalmente comecei a estudar react native!</Text>
+      <Text>Apesar do android emulator ser pesado pra krl!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});

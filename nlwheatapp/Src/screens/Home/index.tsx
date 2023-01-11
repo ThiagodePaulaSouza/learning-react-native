b46bc1350fd1164e8e1d9fd8b34dd80412fcf2b0
ThiagoDePaulaSouza <thiagodepaulasouza@gmail.com>
2023-01-10 22:46:01 -0300

    add: estrutura base

---
+++ nlwheatapp/Src/screens/Home/index.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { styles as s } from './styles'
+
+export default function index() {
+  return (
+    <View style={s.container}>
+      <Text style={s.texto}>Bom dia?</Text>
+    </View>
+  )
+}
+
+
+
---
+++ nlwheatapp/Src/theme/fonts.ts
@@ -0,0 +1,6 @@
+const FONTS = {
+    REGURAR: 'Roboto_400Regular',
+    BOLD: 'Roboto_700Regular',
+}
+
+export default FONTS;
